09b0a85962c692279a92db9b1f037099385c11a4
Killian KOPP <killiankopp@gmail.com>
2025-03-02 17:35:43 +0100

    Add Kafka integration and new API endpoints for object detection

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
-from routers.v1 import router
+from routers.v1 import router as router_v1
+from routers.v2 import router as router_v2
 
 app = FastAPI(
-    title="API Object Detection", version="1.0.0",
+    title="API Object Detection",
+    version="2.0.0",
     description="Détection d'objets dans une image en utilisant un modèle YOLO (prédéfini ou personnalisé)",
 )
 
-app.include_router(router)
+app.include_router(router_v1)
+app.include_router(router_v2)
 
---
+++ routers/v2.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException, UploadFile
+import base64
+
+from src.functions.kafka import send_message
+from src.models.input import FileInput, ModelInput, UriInput, UrlInput
+from src.models.output import ClassInfo, DetectionResult
+from src.yolo_detection import get_model_classes, prediction, uri_file_prediction, url_file_prediction
+
+
+API_TAG_NAME = "object-detection"
+VERSION = "v2"
+api_group_name = f"/{API_TAG_NAME}/{VERSION}/"
+
+router = APIRouter(
+    tags=[api_group_name],
+    prefix=f"/object-detection/{VERSION}"
+)
+
+
+@router.post("/uri")
+async def api_detect_objects_uri( payload: UriInput ) -> DetectionResult:
+    return uri_file_prediction(payload.uri, payload.model_name)
+
+
+@router.post("/url")
+async def api_detect_objects_url( payload: UrlInput ) -> None:
+    send_message('new', {'url': str(payload.url), 'model_name': payload.model_name})
+
+
+@router.get("/model/{model_name}/classes")
+async def api_model_classes(model_name: str) -> list[ClassInfo]:
+    return get_model_classes(model_name)
